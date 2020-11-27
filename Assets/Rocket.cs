49b92ad0df0bacff33c3e9ec555139375cb7c012
RafaF196 <RafaF196@gmail.com>
2020-11-27 15:24:01 +0100

    Video 14

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float mainThrust = 60f;
+    [SerializeField] float rcsThrust = 100f;
 
     Rigidbody rigidBody;
     AudioSource audioSource;
@@ -23,17 +25,31 @@ public class Rocket : MonoBehaviour
         Rotate();
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                // do nothing
+                break;
+            default:
+
+                break;
+        }
+    }
+
     private void Rotate()
     {
         rigidBody.freezeRotation = true;
 
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
         }
 
         rigidBody.freezeRotation = false;
@@ -43,7 +59,7 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
